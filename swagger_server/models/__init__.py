2bf185cc1997bd2ce775cefe53e9de3c696c566e
Christoph Schachinger <christoph@armstrongconsulting.net>
2018-01-14 22:58:26 +0100

    intial commit

---
+++ swagger_server/models/__init__.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+
+# flake8: noqa
+from __future__ import absolute_import
+# import models into model package
+from swagger_server.models.device_info import DeviceInfo
+from swagger_server.models.device_initialization_info import DeviceInitializationInfo
