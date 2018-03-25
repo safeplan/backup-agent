ca42c908247092b42a16ec1877d6223ab3778dd8
Christoph Schachinger <christoph@armstrongconsulting.net>
2018-03-25 16:14:34 +0200

    tests

--- api/test_worker.py
+++ api/test_worker.py
@@ -15,3 +15,10 @@ class TestWorker(unittest.TestCase):
         device_get_details.return_value = StatusInformation(server_time = datetime.utcnow,status='provisioned',as_of=datetime.now() )
         executed_operation = worker.do_work()
         self.assertEqual('noop',executed_operation)
+
+
+     @mock.patch('safeplan_server.device_api.device_get_details')
+     def test2(self, device_get_details):
+        device_get_details.return_value = StatusInformation(server_time = datetime.utcnow,status='provisioned',as_of=datetime.now() )
+        executed_operation = worker.do_work()
+        self.assertEqual('noop',executed_operation)
