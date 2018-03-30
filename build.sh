1474b327488dcd8b8cf9bf9dad5293f4be71694b
Christoph Schachinger <christoph@armstrongconsulting.net>
2018-03-30 09:16:34 +0200

    build

--- build.sh
+++ build.sh
@@ -6,13 +6,23 @@ set -e
 #ng build --prod
 #cd ..
 
+cd fm
+npm install
+gulp build || node node_modules/gulp/bin/gulp.js build
+cd ..
+
 cp fm/index.html api/browse
 
 rm -rf api/browse/dist
 cp -r fm/dist api/browse/dist
 
 rm -rf api/browse/node_modules
-cp -r fm/node_modules api/browse/node_modules
+mkdir -p api/browse/node_modules/jquery/dist && cp fm/node_modules/jquery/dist/jquery.min.js api/browse/node_modules/jquery/dist/jquery.min.js
+mkdir -p api/browse/node_modules/angular && cp fm/node_modules/angular/angular.min.js api/browse/node_modules/angular/angular.min.js
+mkdir -p api/browse/node_modules/angular-translate/dist && cp fm/node_modules/angular-translate/dist/angular-translate.min.js api/browse/node_modules/angular-translate/dist/angular-translate.min.js
+mkdir -p api/browse/node_modules/ng-file-upload/dist && cp fm/node_modules/ng-file-upload/dist/ng-file-upload.min.js api/browse/node_modules/ng-file-upload/dist/ng-file-upload.min.js
+mkdir -p api/browse/node_modules/bootstrap/dist/js && cp fm/node_modules/bootstrap/dist/js/bootstrap.min.js api/browse/node_modules/bootstrap/dist/js/bootstrap.min.js
+mkdir -p api/browse/node_modules/bootswatch/paper && cp fm/node_modules/bootswatch/paper/bootstrap.min.css api/browse/node_modules/bootswatch/paper/bootstrap.min.css
 
 docker build -t safeplan/backup-agent:1 .
 docker push safeplan/backup-agent:1
