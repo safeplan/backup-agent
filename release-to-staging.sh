ba1ff2ad7b865bd8b34c11bdf4219a302924b94c
Roger Armstrong <roger@armstrongconsulting.net>
2019-04-29 10:54:09 +0200

    msg

--- release-to-staging.sh
+++ release-to-staging.sh
@@ -9,6 +9,6 @@ docker push safeplan/backup-agent:STAGING
 
 echo "*****************************************************************"
 echo "You released the :STAGING tagged version of the agent"
-echo "which is only used by specific test devices"
+echo "which is only used by specific test devices (currently just Roger's Synology)"
 echo "To release to production devices, call ./release-to-production.sh"
 echo "*****************************************************************"
